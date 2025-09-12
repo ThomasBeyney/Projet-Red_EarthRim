fa4cf3ada60eb7d25680c8d28fe29432aae7a229
ThomasBeyney <thomas.beyney@ynov.com>
2025-09-12 14:12:52 +0200

    character

--- src/Character.go
+++ src/Character.go
@@ -3,14 +3,30 @@ package main
 import "fmt"
 
 type Character struct {
-	Name String
-	Class String
-	Level Int
-	MaxHp Int
-	Hp Int
-	Inventory String
+	Name      string
+	Class     string
+	Level     int
+	MaxHp     int
+	Hp        int
+	Inventory []string
 }
 
-func initCharacter() {
-	
+func initCharacter() Character {
+	var name string
+	fmt.Print("Quel est le nom de ton personnage ? ")
+	fmt.Scanln(&name)
+
+	return Character{
+		Name:      name,
+		Class:     "Elf",
+		Level:     1,
+		MaxHp:     100,
+		Hp:        40,
+		Inventory: []string{},
+	}
+}
+
+func main() {
+	Player := initCharacter()
+	fmt.Println("Bienvenue", Player.Name)
 }
