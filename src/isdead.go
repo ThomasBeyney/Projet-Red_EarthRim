0e0f61a85bf8f695990c629661264228c8d46940
Elsa13131 <elsachochon@gmail.com>
2025-09-15 12:17:20 +0200

    isdead.go

---
+++ src/isdead.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func isDead(c *Character) {
+	if c.Hp == 0 {
+		fmt.Println("vous etes completement dead")
+		fmt.Println("Mais vous ressuscité, (pas entierement il ne faut pas abuser)")
+		c.Hp = c.MaxHp / 2 
+		fmt.Println("Maintenant vos point de vie sont de : ", c.Hp )
+	}
+}
