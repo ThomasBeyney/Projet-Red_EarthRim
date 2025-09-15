38749aa9674de52834a27236135486d9d465c134
ThomasBeyney <thomas.beyney@ynov.com>
2025-09-15 16:43:52 +0200

    Spellbook

---
+++ src/Spellbook.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func Spellbook(c *Character) {
+	for i, item := range c.Inventory {
+		if item == "Livre de boule de feu" {
+			c.Inventory = append(c.Inventory[:i], c.Inventory[i+1:]...)
+			c.Skill = append(c.Skill, "Boule de feu")
+			fmt.Println("Vous sentez la magie vous envahir !")
+			fmt.Println("Soudenement vous savez comment lancer une boule de feu")
+			fmt.Println("Incroyable !")
+			return
+		}
+	}
+	fmt.Println("Aucune Livre de boule de feu dans l'inventaire")
+}
