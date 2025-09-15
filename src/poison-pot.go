2e241e18a7a9437d09dcf90b339efbf25782832f
ThomasBeyney <thomas.beyney@ynov.com>
2025-09-15 14:28:25 +0200

    poison-pot

--- src/poison-pot.go
+++ src/poison-pot.go
@@ -9,16 +9,19 @@ func poison_pot(c *Character) {
 	for i, item := range c.Inventory {
 		if item == "Potion-de-poison" {
 			c.Inventory = append(c.Inventory[:i], c.Inventory[i+1:]...)
+			fmt.Println("Potion-de-poison utilisée !")
+			fmt.Println("Vous vous santez un peut bizzard")
+			fmt.Println("vous perdez 10 points de vie, aïe")
 			c.Hp -= 10
 			time.Sleep(1 * time.Second)
+			fmt.Println("vous reperdez 10 points de vie")
 			c.Hp -= 10
 			time.Sleep(1 * time.Second)
+			fmt.Println("les effets du poison disparaisse mais vous perdez encore 10 points de vie")
 			c.Hp -= 10
 			if c.Hp < 0 {
 				c.Hp = 0
 			}
-			fmt.Println("Potion-de-poison utilisée !")
-			fmt.Println("Vous vous santez un peut bizzard")
 			fmt.Println("Points de vie :", c.Hp, "/", c.MaxHp)
 			return
 		}
