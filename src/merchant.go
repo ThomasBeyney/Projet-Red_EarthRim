828307c2e68a625fbc1520231b7843f728346020
ThomasBeyney <thomas.beyney@ynov.com>
2025-09-15 12:01:10 +0200

    marchand

--- src/merchant.go
+++ src/merchant.go
@@ -5,12 +5,14 @@ import "fmt"
 func Marchand(c *Character) {
 	fmt.Println("Bienvenue au Marchand")
 	var reponse string
-	fmt.Print("Voulait vous achetez une potion ?")
+	fmt.Print("Voulait vous achetez une potion ?  oui/non ")
 	fmt.Scanln(&reponse)
 
 	if reponse == "oui" || reponse == "Oui" {
 		fmt.Println("Vous obtenez une potion")
-	} else {
+	} else if reponse == "non" || reponse == "Non" {
 		fmt.Println("Dommage, elle pourrait ètre utile")
+	} else {
+		fmt.Println("Et bien, ce n'était pas ma question")
 	}
 }
