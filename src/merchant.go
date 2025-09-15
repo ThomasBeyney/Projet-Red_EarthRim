6bf7460a8d49cb7ac21a2e7bbc08604afbaf2282
ThomasBeyney <thomas.beyney@ynov.com>
2025-09-15 12:57:38 +0200

    marchand

--- src/merchant.go
+++ src/merchant.go
@@ -4,16 +4,42 @@ import "fmt"
 
 func Marchand(c *Character) {
 	fmt.Println("Bienvenue au Marchand")
+	var rep int
 	var reponse string
-	fmt.Print("Voulait vous achetez une potion ?  oui/non ")
-	fmt.Scanln(&reponse)
+	for {
+		fmt.Println("1- voulez vous achetez une potion de soin ?")
+		fmt.Println("2- voulez vous achetez une potion de poison ?")
+		fmt.Println("3- Partir")
+		fmt.Print("Choix : ")
+		fmt.Scanln(&rep)
+		switch rep {
+		case 1 :
+			fmt.Print("Voulait vous vraiment achetez une potion de soin ?  oui/non ")
+			fmt.Scanln(&reponse)
 
-	if reponse == "oui" || reponse == "Oui" {
-		fmt.Println("Vous obtenez une potion")
-		c.Inventory = append(c.Inventory, "Potion")
-	} else if reponse == "non" || reponse == "Non" {
-		fmt.Println("Dommage, elle pourrait ètre utile")
-	} else {
-		fmt.Println("Et bien, ce n'était pas ma question")
+			if reponse == "oui" || reponse == "Oui" {
+				fmt.Println("Vous obtenez une potion")
+				c.Inventory = append(c.Inventory, "Potion")
+			} else if reponse == "non" || reponse == "Non" {
+				fmt.Println("Dommage, elle pourrait ètre utile")
+			} else {
+				fmt.Println("Et bien, ce n'était pas ma question")
+			}
+		case 2 :
+			fmt.Print("Voulait vous vraiment achetez une potion de poison ? oui/non ")
+			fmt.Scanln(&reponse)
+
+			if reponse == "oui" || reponse == "Oui" {
+				fmt.Println("Vous obtenez une potion de poison")
+				c.Inventory = append(c.Inventory, "Potion-de-poison")
+			} else if reponse == "non" || reponse == "Non" {
+				fmt.Println("Dommage, elle pourrait ètre utile, ou mortel")
+			} else {
+				fmt.Println("Et bien, ce n'était pas ma question")
+			}
+		case 3 :
+			fmt.Println("A biientot l'ami !")
+			return
+		}
 	}
 }
