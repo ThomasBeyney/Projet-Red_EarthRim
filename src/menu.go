5879249533cf6c89805121b81ee1ddd04156db81
Elsa13131 <elsachochon@gmail.com>
2025-09-16 09:58:07 +0200

    menu.go

--- src/menu.go
+++ src/menu.go
@@ -11,8 +11,8 @@ func main() {
 		fmt.Println("\n=== Menu Principal ===")
 		fmt.Println("1 - Afficher les informations du personnage")
 		fmt.Println("2 - Accéder au contenu de l'inventaire")
-		fmt.Println("3 - Utiliser une potion de soin")
-		fmt.Println("4 - Utiliser une potion de poison")
+		fmt.Println("3 - Utiliser un objet")
+		fmt.Println("4 - Forgeron")
 		fmt.Println("5 - Marchand")
 		fmt.Println("6 - Quitter")
 		fmt.Print("Choix : ")
@@ -25,9 +25,52 @@ func main() {
 		case 2:
 			accessInventory(&player)
 		case 3:
-			takePot(&player)
+		var rep2 int
+    for {
+        fmt.Println("Que voulez-vous faire ?")
+        fmt.Println("1 - Voir Inventaire")
+        fmt.Println("2 - Revenir en arrière")
+        fmt.Print("Choix : ")
+        fmt.Scanln(&rep2)
+
+        if rep2 == 2 {
+            break
+        }
+
+        if len(player.Inventory) == 0 {
+            fmt.Println("Votre inventaire est vide.")
+            continue
+        }
+
+        fmt.Println("=== Inventaire ===")
+        for i, item := range player.Inventory {
+            fmt.Println(i+1, "-", item)
+        }
+
+        var itemChoice int
+        fmt.Print("Quel objet voulez-vous utiliser ? ")
+        fmt.Scanln(&itemChoice)
+
+        if itemChoice < 1 || itemChoice > len(player.Inventory) {
+            fmt.Println("Choix invalide.")
+            continue
+        }
+
+        selectedItem := player.Inventory[itemChoice-1]
+
+        switch selectedItem {
+        case "Potion":
+            takePot(&player)
+        case "Potion de poison":
+            poison_pot(&player)
+		case "Livre de boule de feu": 
+			Spellbook(&player)
+        default:
+            fmt.Println("Cet objet ne peut pas être utilisé.")
+        }
+		}
 		case 4:
-			poison_pot(&player)
+			Forgeron(&player)
 		case 5:
 			Marchand(&player)
 		case 6:
