ede976898adee4e858c762163494c9ef40d3c9c3
Elsa13131 <elsachochon@gmail.com>
2025-09-16 09:55:51 +0200

    monster.go

---
+++ src/monster.go
@@ -0,0 +1,17 @@
+package main 
+
+type Monster struct {
+	Name      string
+	MaxHp     int
+	Hp        int
+	attaque   int
+}
+
+func initGoblin() Monster {
+	return Character{
+		Name:      "Gobelin d'entrainement",
+		MaxHp:     40,
+		Hp:        40,
+		attaque:   5
+	}
+}
