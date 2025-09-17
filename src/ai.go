3cd47b74f544c93f2ec7b072a5c7102099d20357
ThomasBeyney <thomas.beyney@ynov.com>
2025-09-17 16:26:57 +0200

    ai

--- src/ai.go
+++ src/ai.go
@@ -40,3 +40,108 @@ func goblinPattern(goblin *Monster, player *Character) {
 		CharacterTurn(goblin, player)
 	}
 }
+
+func trollPattern(Troll *Monster, player *Character) {
+	for turn := 1; player.Hp > 0; turn++ {
+		if Troll.Hp <= 0 {
+			return
+		}
+
+		var damage int
+		if player.Turn%10 == 0 {
+			damage = Troll.attaque * 10
+			fmt.Println(Troll.Name, "utilise une attaque MORTEL !")
+		} else {
+			damage = Troll.attaque
+		}
+
+		player.Hp -= damage
+		if player.Hp < 0 {
+			player.Hp = 0
+		}
+
+		fmt.Println(Troll.Name, "inflige", damage, "dégâts à", player.Name)
+		fmt.Println("PV de", player.Name, ":", player.Hp, "/", player.MaxHp)
+		time.Sleep(1 * time.Second)
+
+		if player.Hp == 0 {
+			fmt.Println(player.Name, "est K.O.")
+			time.Sleep(1 * time.Second)
+			isDead(player)
+			return
+		}
+
+		player.Turn += 1
+		CharacterTurn(Troll, player)
+	}
+}
+
+func loupPattern(Loup *Monster, player *Character) {
+	for turn := 1; player.Hp > 0; turn++ {
+		if Loup.Hp <= 0 {
+			return
+		}
+
+		var damage int
+		if player.Turn%3 == 0 {
+			damage = Loup.attaque
+		} else {
+			damage = Loup.attaque
+		}
+
+		player.Hp -= damage
+		if player.Hp < 0 {
+			player.Hp = 0
+		}
+
+		fmt.Println(Loup.Name, "inflige", damage, "dégâts à", player.Name)
+		fmt.Println("PV de", player.Name, ":", player.Hp, "/", player.MaxHp)
+		time.Sleep(1 * time.Second)
+
+		if player.Hp == 0 {
+			fmt.Println(player.Name, "est K.O.")
+			time.Sleep(1 * time.Second)
+			isDead(player)
+			return
+		}
+
+		player.Turn += 1
+		CharacterTurn(Loup, player)
+	}
+}
+
+
+func orcPattern(Orc *Monster, player *Character) {
+	for turn := 1; player.Hp > 0; turn++ {
+		if Orc.Hp <= 0 {
+			return
+		}
+
+		var damage int
+		if player.Turn%5 == 0 {
+			damage = Orc.attaque * 3
+			fmt.Println(Orc.Name, "utilise une attaque puissante !")
+		} else {
+			damage = Orc.attaque
+		}
+
+		player.Hp -= damage
+		if player.Hp < 0 {
+			player.Hp = 0
+		}
+
+		fmt.Println(Orc.Name, "inflige", damage, "dégâts à", player.Name)
+		fmt.Println("PV de", player.Name, ":", player.Hp, "/", player.MaxHp)
+		time.Sleep(1 * time.Second)
+
+		if player.Hp == 0 {
+			fmt.Println(player.Name, "est K.O.")
+			time.Sleep(1 * time.Second)
+			isDead(player)
+			return
+		}
+
+		player.Turn += 1
+		CharacterTurn(Orc, player)
+	}
+}
