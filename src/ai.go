fa3ca8a171569fd6545f8f19c81aaf29d4028408
ThomasBeyney <thomas.beyney@ynov.com>
2025-09-17 12:43:29 +0200

    ai

--- src/ai.go
+++ src/ai.go
@@ -5,28 +5,32 @@ import (
 	"time"
 )
 
-func goblinPattern(goblin Monster, playerCharacter, turn int) {
-	var damage int
 
-	if turn%3 == 0 {
-		damage = goblin.attaque * 2
-		fmt.Println(goblin.Name, "utilise une attaque puissante !")
-	} else {
-		damage = goblin.attaque
-	}
+func goblinPattern(goblin *Monster, player *Character) {
+	for turn := 1; player.Hp > 0; turn++ {
+		var damage int
+		if turn%3 == 0 {
+			damage = goblin.attaque * 2
+			fmt.Println(goblin.Name, "utilise une attaque puissante !")
+		} else {
+			damage = goblin.attaque
+		}
 
-	player.Hp -= damage
-	if player.Hp < 0 {
-		player.Hp = 0
-	}
 
-	fmt.Println(goblin.Name, "inflige", damage, "dégâts à", player.Name)
-	fmt.Println("PV de", player.Name, ":", player.Hp, "/", player.MaxHp)
-	time.Sleep(1 * time.Second)
+		player.Hp -= damage
+		if player.Hp < 0 {
+			player.Hp = 0
+		}
 
-	if player.Hp == 0 {
-		fmt.Println(player.Name, "est K.O.")
+		fmt.Println(goblin.Name, "inflige", damage, "dégâts à", player.Name)
+		fmt.Println("PV de", player.Name, ":", player.Hp, "/", player.MaxHp)
 		time.Sleep(1 * time.Second)
-		isDead(player)
+
+		if player.Hp == 0 {
+			fmt.Println(player.Name, "est K.O.")
+			time.Sleep(1 * time.Second)
+			isDead(player)
+		}
+	CharacterTurn(goblin, player)
 	}
 }
