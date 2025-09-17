ff7dc9beeeefa41359e1f6dc3547ec85064b4289
Elsa13131 <elsachochon@gmail.com>
2025-09-17 15:41:26 +0200

    monts

--- src/monster.go
+++ src/monster.go
@@ -16,3 +16,29 @@ func initGoblin() Monster {
 	}
 }
 
+func initTroll() Monster {
+	return Monster{
+		Name:    "Troll"
+		MaxHp:   80,
+		Hp:      80,
+		attaque: 10,
+	}
+}
+
+func initLoup() Monster {
+	return Monster{
+		Name:    "Loup"
+		MaxHp:   "25"
+		Hp:      "25"
+		attaque: "3"
+	}
+}
+
+func initOrc() Monster {
+	return Monster{
+		Name:    "Orc"
+		MaxHp:   "50"
+		Hp:      "50"
+		attaque: "5"
+	}
+}
