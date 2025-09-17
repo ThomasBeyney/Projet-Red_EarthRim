3b83dd6c00586cb95a2df971084ff08a20bfcff9
ThomasBeyney <thomas.beyney@ynov.com>
2025-09-17 11:01:28 +0200

    ai

--- src/ai.go
+++ src/ai.go
@@ -1,14 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
 
 func goblinPattern(goblin *Monster, player *Character) {
 	for turn := 1; player.Hp > 0; turn++ {
 		var damage int
 		if turn%3 == 0 {
 			damage = goblin.attaque * 2
+			CharacterTurn(goblin, player)
 		} else {
 			damage = goblin.attaque
+			CharacterTurn(goblin, player)
 		}
 
 		player.Hp -= damage
@@ -18,9 +24,12 @@ func goblinPattern(goblin *Monster, player *Character) {
 		}
 
 		fmt.Println(goblin.Name,"inflige à",player.Name,damage,"points de dégâts\n")
+		time.Sleep(1 * time.Second)
 		fmt.Println("PV de",player.Name,":",player.Hp,"/",player.MaxHp,"\n\n")
+		time.Sleep(1 * time.Second)
 
 		if player.Hp == 0 {
+			time.Sleep(1 * time.Second)
 			fmt.Println(player.Name," est K.O.\n")
 			isDead(player)
 			break
