c4bbad65e78d29cf071aff87480792c74934c6a1
ThomasBeyney <thomas.beyney@ynov.com>
2025-09-17 09:29:00 +0200

    Spellbook

--- src/Spellbook.go
+++ src/Spellbook.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func Spellbook(c *Character) {
+	if c.Skillmax > 1 {
+		fmt.Println("vous avez déja appris ce sort")
+		return
+	}
 	for i, item := range c.Inventory {
 		if item == "Livre de boule de feu" {
 			c.Inventory = append(c.Inventory[:i], c.Inventory[i+1:]...)
@@ -10,8 +14,9 @@ func Spellbook(c *Character) {
 			fmt.Println("Vous sentez la magie vous envahir !")
 			fmt.Println("Soudenement vous savez comment lancer une boule de feu")
 			fmt.Println("Incroyable !")
+			c.Skillmax++
 			return
 		}
 	}
 	fmt.Println("Aucune Livre de boule de feu dans l'inventaire")
-}
+	}
