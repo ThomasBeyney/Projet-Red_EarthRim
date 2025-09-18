c502bb750404fbfdbfb8a84641e8d295427bfbe0
ThomasBeyney <thomas.beyney@ynov.com>
2025-09-18 12:49:40 +0200

    lore

--- src/lore.go
+++ src/lore.go
@@ -5,11 +5,16 @@ import (
     "time"
 )
 
-func displayLore(lore string, delayMs int) {
+func displayLore(lore string, delayMs int) bool {
     for _, r := range lore {
         fmt.Printf("%c", r)
         time.Sleep(time.Duration(delayMs) * time.Millisecond)
     }
     fmt.Println()
-	characterCreation()
+    return true
+}
+
+func main() {
+    result := displayLore("Mon texte, voici un grand texte qui ne sert pas a grand chose mais c'est pas grave, en vrai c'est juste un test.", 30)
+    fmt.Println("Fin du lore ?", result)
 }
