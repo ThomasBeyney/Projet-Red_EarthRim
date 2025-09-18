9d4a2e4fb6b2684132aba5158223c9dd5b0b90fd
Elsa13131 <elsachochon@gmail.com>
2025-09-18 10:50:34 +0200

    menu

--- src/menu.go
+++ src/menu.go
@@ -9,7 +9,9 @@ import (
 
 func main() {
 	player :=  characterCreation()
+	fmt.Println(" ")
 	fmt.Println("Bienvenue", player.Name, "tu as", player.Hp, "/", player.MaxHp, "hp")
+	fmt.Println(" ")
 
 	var choice int
 	for {
@@ -21,6 +23,7 @@ func main() {
 		fmt.Println("5 - Marchand")
 		fmt.Println("6 - Combat")
 		fmt.Println("7 - Quitter")
+		fmt.Println(" ")
 		fmt.Print("Choix : ")
 		fmt.Scanln(&choice)
 
@@ -33,9 +36,11 @@ func main() {
 		case 3:
 		var rep2 int
     for {
+		fmt.Println(" ")
         fmt.Println("Que voulez-vous faire ?")
         fmt.Println("1 - Voir Inventaire")
         fmt.Println("2 - Revenir en arrière")
+		fmt.Println(" ")
         fmt.Print("Choix : ")
         fmt.Scanln(&rep2)
 
@@ -58,6 +63,7 @@ func main() {
         fmt.Scanln(&itemChoice)
 
         if itemChoice < 1 || itemChoice > len(player.Inventory) {
+			fmt.Println(" ")
             fmt.Println("Choix invalide.")
             continue
         }
@@ -90,10 +96,12 @@ func main() {
 		case 6:
 		var rep3 int
 			for {
+				fmt.Println(" ")
 				fmt.Println("Que voulez-vous faire ?")
 				fmt.Println("1 - Combat d'entrainement")
 				fmt.Println("2 - Combat")
 				fmt.Println("3 - Revenir en arrière")
+				fmt.Println(" ")
 				fmt.Print("Choix : ")
 				fmt.Scanln(&rep3)
 
@@ -115,9 +123,11 @@ func main() {
 							CombTroll(&troll, &player)
 						}
 					case 3:
+						fmt.Println(" ")
 						fmt.Println("A plus tard Combattant !")
 						break
 					default:
+						fmt.Println(" ")
 					fmt.Println("Choix invalide, veuillez réessayer.")
 					}
 
@@ -127,9 +137,11 @@ func main() {
 				}
 
 		case 7:
+			fmt.Println(" ")
 			fmt.Println("Au revoir !")
 			return
 		default:
+			fmt.Println(" ")
 			fmt.Println("Choix invalide, veuillez réessayer.")
 		}
 	}
