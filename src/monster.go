da0cbc3ed0074bb07d13d48ae694879f9503a798
Elsa13131 <elsachochon@gmail.com>
2025-09-18 09:12:46 +0200

    mons

--- src/monster.go
+++ src/monster.go
@@ -18,7 +18,7 @@ func initGoblin() Monster {
 
 func initTroll() Monster {
 	return Monster{
-		Name:    "Troll"
+		Name:    "Troll",
 		MaxHp:   80,
 		Hp:      80,
 		attaque: 10,
@@ -27,18 +27,18 @@ func initTroll() Monster {
 
 func initLoup() Monster {
 	return Monster{
-		Name:    "Loup"
-		MaxHp:   "25"
-		Hp:      "25"
-		attaque: "3"
+		Name:    "Loup",
+		MaxHp:   25,
+		Hp:      25,
+		attaque: 3,
 	}
 }
 
 func initOrc() Monster {
 	return Monster{
-		Name:    "Orc"
-		MaxHp:   "50"
-		Hp:      "50"
-		attaque: "5"
+		Name:    "Orc",
+		MaxHp:   50,
+		Hp:      50,
+		attaque: 5,
 	}
 }
